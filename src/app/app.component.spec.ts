b260950e3270443dfbe28a12f145c0359e463f83
Stanko Arbutina <stanko.arbutina@gmail.com>
2020-04-11 13:30:33 +0200

    Initialize chromatic

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,6 @@ import { TestBed, async } from '@angular/core/testing';
 import { NgxsModule, Store } from '@ngxs/store';
 
 import {CommonModule} from '@angular/common';
-import {AppRoutingModule} from './app-routing.module';
 import {TaskModule} from './components/task.module';
 import {TasksState} from './state/task.state';
 
